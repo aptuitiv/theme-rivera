f1e2e1c64386e2b74bda5a84dc09c454c6ed4f8e
Matthew Gallant <mgallant@aptuitiv.com>
2021-04-20 08:47:34 -0400

    Remove Modernizr

--- gulp/config.js
+++ gulp/config.js
@@ -147,7 +147,6 @@ module.exports = {
         {
             name: 'main.js',
             src: [
-                src + '/js/modernizr-flexbox-detection.js',
                 'node_modules/jquery/dist/jquery.js',
                 src + '/js/main.js'
             ]
